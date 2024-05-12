1cc1ffb3ebaadfca6ad9cc4444bd07481f9fab0c
Justin Wagoner <justin.wagoner@exchange.koriist.com>
2024-05-12 13:31:26 -0600

    added drone class

---
+++ Endless Survival/Drone.swift
@@ -0,0 +1,53 @@
+import SpriteKit
+
+class Drone: Codable {
+    var radius: CGFloat
+    var fireRate: TimeInterval
+    var damage: CGFloat
+    var lastAttackTime: TimeInterval
+
+    enum CodingKeys: String, CodingKey {
+        case radius
+        case fireRate
+        case damage
+        case lastAttackTime
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(radius, forKey: .radius)
+        try container.encode(fireRate, forKey: .fireRate)
+        try container.encode(damage, forKey: .damage)
+        try container.encode(lastAttackTime, forKey: .lastAttackTime)
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let radius = try container.decode(CGFloat.self, forKey: .radius)
+        let fireRate = try container.decode(TimeInterval.self, forKey: .fireRate)
+        let damage = try container.decode(CGFloat.self, forKey: .damage)
+        let lastAttackTime = try container.decode(TimeInterval.self, forKey: .lastAttackTime)
+
+        self.radius = radius
+        self.fireRate = fireRate
+        self.damage = damage
+        self.lastAttackTime = lastAttackTime
+    }
+    
+    init(radius: CGFloat, fireRate: TimeInterval, damage: CGFloat) {
+        self.radius = radius
+        self.fireRate = fireRate
+        self.damage = damage
+        self.lastAttackTime = 0
+    }
+}
+
+class AssaultDrone: Drone {
+    init() {
+        super.init(radius: 100, fireRate: 2, damage: 2)
+    }
+    
+    required init(from decoder: Decoder) throws {
+        fatalError("init(from:) has not been implemented")
+    }
+}
