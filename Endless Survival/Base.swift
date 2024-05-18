d633e0c6ad9a6c4efd1557c041c66987a9e5494d
Justin Wagoner <justin.wagoner@exchange.koriist.com>
2024-05-18 11:08:04 -0600

    updated base barrier size and component size, set groundwork for future baselevels to make size easier

--- Endless Survival/Base.swift
+++ Endless Survival/Base.swift
@@ -1,17 +1,23 @@
 import SpriteKit
 
 class Base: SKSpriteNode, Codable {
+    let baseLevel2ComponentSize: Int = 75
+    let baseLevel2TotalSize: Int = 225
+
+    var baseLevel: Int = 1
     var components: [BaseComponent] = []
-    var barrier = BaseBarrier(size: CGSize(width: 400, height: 400), strokeColor: .black, lineWidth: 2.0)
+    var barrier: BaseBarrier!
 
     enum CodingKeys: String, CodingKey {
         case components
         case barrier
+        case baseLevel
     }
 
     init() {
         super.init(texture: nil, color: .white, size: CGSize(width: 75, height: 75))
         self.zPosition = 2
+        self.barrier = BaseBarrier(size: CGSize(width: baseLevel2TotalSize + 100, height: baseLevel2TotalSize + 100), strokeColor: .black, lineWidth: 2.0)
         self.addChild(barrier)
     }
 
@@ -19,12 +25,14 @@ class Base: SKSpriteNode, Codable {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(components, forKey: .components)
         try container.encode(barrier, forKey: .barrier)
+        try container.encode(baseLevel, forKey: .baseLevel)
     }
 
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         components = try container.decode([BaseComponent].self, forKey: .components)
         barrier = try container.decode(BaseBarrier.self, forKey: .barrier)
+        baseLevel = try container.decode(Int.self, forKey: .baseLevel)
         super.init(texture: nil, color: .white, size: CGSize(width: 75, height: 75))
     }
     
@@ -35,14 +43,14 @@ class Base: SKSpriteNode, Codable {
 
     func positionComponents() {
         let offsets = [
-            CGPoint(x: -75, y: 75),   // Position 1
-            CGPoint(x: -75, y: 0),    // Position 2
-            CGPoint(x: -75, y: -75),  // Position 3
-            CGPoint(x: 0, y: -75),    // Position 4
-            CGPoint(x: 75, y: 75),    // Position 5
-            CGPoint(x: 75, y: 0),     // Position 6
-            CGPoint(x: 75, y: -75),   // Position 7
-            CGPoint(x: 0, y: 75)      // Position 8
+            CGPoint(x: -baseLevel2ComponentSize, y: baseLevel2ComponentSize),   // Position 1
+            CGPoint(x: -baseLevel2ComponentSize, y: 0),    // Position 2
+            CGPoint(x: -baseLevel2ComponentSize, y: -baseLevel2ComponentSize),  // Position 3
+            CGPoint(x: 0, y: -baseLevel2ComponentSize),    // Position 4
+            CGPoint(x: baseLevel2ComponentSize, y: baseLevel2ComponentSize),    // Position 5
+            CGPoint(x: baseLevel2ComponentSize, y: 0),     // Position 6
+            CGPoint(x: baseLevel2ComponentSize, y: -baseLevel2ComponentSize),   // Position 7
+            CGPoint(x: 0, y: baseLevel2ComponentSize)      // Position 8
         ]
 
         for (index, component) in components.enumerated() {
